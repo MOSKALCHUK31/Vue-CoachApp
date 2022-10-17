e9e6958daf983690ee433bc2d33f0600bb166788
MOSKALCHUK <moskalchuk1338@gmail.com>
2022-10-17 10:52:09 +0300

    Connect all components

--- src/pages/coaches/CoachList.vue
+++ src/pages/coaches/CoachList.vue
@@ -1,3 +1,12 @@
 <template>
-    COACHLIST
+    <section>
+        FILTER
+    </section>
+    <section>
+        <div class="controls">
+            <button>Refresh</button>
+            <router-link to="/register">Register as Coach</router-link>
+        </div>
+        <ul>LIST OF COACHES</ul>
+    </section>
 </template>
