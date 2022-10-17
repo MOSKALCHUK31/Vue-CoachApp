5fa7bedc76cbfd59c968b4bb3cd272c077c01d66
MOSKALCHUK <moskalchuk1338@gmail.com>
2022-10-17 11:52:41 +0300

    Add VUEX and coach data

--- src/pages/coaches/CoachList.vue
+++ src/pages/coaches/CoachList.vue
@@ -7,6 +7,20 @@
             <button>Refresh</button>
             <router-link to="/register">Register as Coach</router-link>
         </div>
-        <ul>LIST OF COACHES</ul>
+        <ul v-if="hasCoaches">
+            <li v-for="coach in coaches" :key="coach.id">{{ coach.firstName }}</li>
+        </ul>
+        <h3 v-else>There is no coahes found</h3>
     </section>
 </template>
+
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+    computed: {
+        ...mapGetters('coaches', ['coaches', 'hasCoaches']),
+    }
+}
+</script>
