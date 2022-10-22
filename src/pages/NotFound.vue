6dcc3feaf1de58e06ccdba95bf3924e6a6ea67db
MOSKALCHUK <moskalchuk1338@gmail.com>
2022-10-22 15:08:48 +0300

    Add not found page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <section>
+        <base-card>
+            <h2>Page not found!</h2>
+            <p>This page shouldn't be found - maybe check out <router-link to="/">our coaches</router-link></p>
+        </base-card>
+    </section>
+</template>
