08000dcfd87320ae38727761873a711e7a992883
Sergey Moskalchuk <66484544+MOSKALCHUK31@users.noreply.github.com>
2022-12-15 12:33:59 +0200

    Optimization [ Add Async components ]

--- src/router.js
+++ src/router.js
@@ -1,11 +1,12 @@
+import { defineAsyncComponent } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
-import CoachList from './pages/coaches/CoachList.vue'
-import CoachDetails from './pages/coaches/CoachDetails.vue'
-import CoachRegistration from './pages/coaches/CoachRegistration.vue'
-import ContactCoach from './pages/requests/ContactCoach.vue'
-import RequestsReceived from './pages/requests/RequestsReceived.vue'
-import UserAuth from './pages/auth/UserAuth.vue'
+const CoachList = defineAsyncComponent(() => import('./pages/coaches/CoachList.vue'))
+const CoachDetails = defineAsyncComponent(() => import('./pages/coaches/CoachDetails.vue'))
+const CoachRegistration = defineAsyncComponent(() => import('./pages/coaches/CoachRegistration.vue'))
+const ContactCoach = defineAsyncComponent(() => import('./pages/requests/ContactCoach.vue'))
+const RequestsReceived = defineAsyncComponent(() => import('./pages/requests/RequestsReceived.vue'))
+const UserAuth = defineAsyncComponent(() => import('./pages/auth/UserAuth.vue'))
 import NotFound from './pages/NotFound.vue'
 import store from './store/store'
 
